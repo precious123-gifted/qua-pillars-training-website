a3e4b8dc06f27b8f0f97f2cd908fb8dbaa3a0506
precious123-gifted <ogunameprecious123@gmail.com>
2025-03-03 13:26:07 +0100

    restyle

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 export default function HomePage() {
   return (
-    <div className='home-page w-full landscape:h-[110vh] bg-[#e9ebf0] font-[family-name:var(--font-kanit)] grid place-items-center   '>
+    <div className='home-page w-full landscape:h-[120vh] bg-[#e9ebf0] font-[family-name:var(--font-kanit)] grid place-items-center   '>
     <div className="content w-[95%]  overflow-hidden landscape:space-y-[1vw] portrait:space-y-[6vw] ">
 {/* header banner */}
 <header
@@ -28,7 +28,14 @@ className='flex justify-center pt-[0vw]  h-[60vh]  text-[#b8c2d8] rounded-sm por
 {/* why attend */}
 <section className='text-[#3b4a6e] flex flex-col items-center landscape:text-[1.2vw] portrait:text-center mt-[4vw] '>
 <h3 className='landscape:text-[1.6vw] mb-4'>Why Attend?</h3>
-<p>This Evidence-Based Medicines Research and Pharmacovigilance Training offers a comprehensive and hands-on learning experience inthe rapidly evolving fields of medicine research and pharmacovigilance. It provides an excellent opportunity for professionals to stayinformed about study design, ethical considerations, patient safety, and regulatory practices. Led by experts, the program exploresmedicine research and pharmacovigilance principles and practices. Participants will gain insights into adverse event reporting,medication errors, and safety monitoring for vaccines, biologics, and herbal medicines. The training also covers practical projectmanagement and publication writing skills, including the use of AI. A key feature of this training is the roundtable discussion onmedicines research in Africa, where professionals will share insights, lessons learned, and future directions. This program is ideal forresearchers, healthcare professionals, and regulatory authorities who aim to enhance their skills, expand their professional network,and contribute to advancing medicines research and pharmacovigilance.</p>
+<p className='mb-[2vw]'>This Evidence-Based Medicines Research and Pharmacovigilance Training offers a comprehensive and hands-on learning experience inthe rapidly evolving fields of medicine research and pharmacovigilance. It provides an excellent opportunity for professionals to stayinformed about study design, ethical considerations, patient safety, and regulatory practices. Led by experts, the program exploresmedicine research and pharmacovigilance principles and practices. 
+</p>
+<p className='mb-[2vw]'>Participants will gain insights into adverse event reporting,medication errors, and safety monitoring for vaccines, biologics, and herbal medicines. The training also covers practical projectmanagement and publication writing skills, including the use of AI. A key feature of this training is the roundtable discussion onmedicines research in Africa, where professionals will share insights, lessons learned, and future directions. 
+
+</p>
+<p className='mb-[2vw]'>
+This program is ideal forresearchers, healthcare professionals, and regulatory authorities who aim to enhance their skills, expand their professional network,and contribute to advancing medicines research and pharmacovigilance.
+</p>
 </section>
 
 
