c2e1e996590ca43209fe9075ce713903ae30afbb
precious123-gifted <ogunameprecious123@gmail.com>
2025-03-03 10:37:51 +0100

    restyling

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Enable static export
   distDir: 'out', 
-  basePath: '/',
+  basePath: '',
   images: {
     unoptimized: false, // Disable Image Optimization API
   },
