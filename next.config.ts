285ef9138a4eb011c6cf74b46d66a0b6ba7b866d
precious123-gifted <ogunameprecious123@gmail.com>
2025-03-03 10:48:47 +0100

    restyling

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Enable static export
-  distDir: 'out', 
-  basePath: '',
-  images: {
-    unoptimized: false, // Disable Image Optimization API
-  },
-};
 
+};
 export default nextConfig;
