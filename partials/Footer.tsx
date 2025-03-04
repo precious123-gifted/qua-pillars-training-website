df5e657653a50a3525af8133f08e8b55d31c19ba
precious123-gifted <ogunameprecious123@gmail.com>
2025-03-04 11:59:49 +0100

    elegance

--- partials/Footer.tsx
+++ partials/Footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-      <footer className="grid place-items-center bg-[#19294c] text-[#9fabc5] py-3 ">
+      <footer className="grid place-items-center bg-[#19294c] text-[#9fabc5] py-3 portrait:text-[1.2vw] ">
         <p>Copyright © 2025 Qua Pillar Health Research Foundation | All rights reserved. Privacy Policy</p>
       </footer>
     );
